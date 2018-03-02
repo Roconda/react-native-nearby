10ca814d258cbd647c2a67ab4470a687985a5e3c
Tim Slot <tim.slot@gmail.com>
2018-03-02 09:38:27 +0100

    implement basic automatic beacon subscribing

--- ios/RNNearby.m
+++ ios/RNNearby.m
@@ -1,5 +1,8 @@
-
 #import "RNNearby.h"
+
+NSString *_apiKey = nil;
+GNSMessageManager *_msgManager = nil;
+id<GNSSubscription> _msgSubscription = nil;
 
 @implementation RNNearby
 
@@ -9,5 +12,42 @@
 }
 RCT_EXPORT_MODULE()
 
+- (instancetype)initWithApiKey:(nonnull NSString*)apiKey {
+    self = [super init];
+    if (self != nil) {
+        NSLog(@"RNNearby initiating");
+        _apiKey = apiKey;
+        NSLog(@"RNNearby apiKey: %@", apiKey);
+        [self subscribe];
+    }
+    return self;
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"RNNearby_FOUND", @"RNNearby_LOST"];
+}
+
+- (void)sendEventWithName:(NSString*)name body:(GNSMessage*)message {
+    NSString *receivedDataString = [[NSString alloc] initWithData:message.content encoding:NSUTF8StringEncoding];
+    NSLog(@"RNNearby received data %@",receivedDataString);
+}
+
+- (void)subscribe {
+    NSLog(@"RNNearby subscribing");
+    _msgManager = [[GNSMessageManager alloc] initWithAPIKey:_apiKey];
+    _msgSubscription = [_msgManager subscriptionWithMessageFoundHandler:^(GNSMessage *message) {
+        NSLog(@"RNNearby msg found!");
+
+        [self sendEventWithName:@"RNNearby_FOUND" body:message];
+    } messageLostHandler:^(GNSMessage *message) {
+        NSLog(@"RNNearby msg lost!");
+    } paramsBlock:^(GNSSubscriptionParams *params) {
+        params.deviceTypesToDiscover = kGNSDeviceBLEBeacon;
+        params.beaconStrategy = [GNSBeaconStrategy strategyWithParamsBlock:^(GNSBeaconStrategyParams *params) {
+            params.includeIBeacons = NO;
+            params.lowPowerPreferred = NO;
+        }];
+    }];
+}
+
 @end
-  
